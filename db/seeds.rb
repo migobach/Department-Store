fde906d827436dc5dd854bb720d5d1571a67983c
Michael Bach <migobach@gmail.com>
2018-05-30 17:02:41 -0600

    add more crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  category = Category.create(
+    name: Faker::Commerce.department
+  )
+  20.times do
+    category.items.create(
+      name: Faker::Commerce.product_name,
+      description: Faker::Commerce.material, 
+      price: Faker::Commerce.price,
+      available: [true, false].sample
+    )
+  end
+end
+
+puts "seeded"
